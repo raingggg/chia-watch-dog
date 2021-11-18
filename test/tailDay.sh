66ab7c470fdbd7a693e0526318d875ae42c84166
raingggg <raingggg@nomail.com>
2021-11-18 09:49:49 +0800

    dailylog and evs array fix

---
+++ test/tailDay.sh
@@ -0,0 +1,53 @@
+LOG_FILE_NAME=~/.chia/mainnet/log/debug.log
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
+
+# head lines to trigger the event
+echo "line 1 trigger day stat events" >>${LOG_FILE_NAME}
+sleep 1m
